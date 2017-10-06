cffe1fc8a51eb458b5656012b6c9ec8ee36c246d
vagrant <vagrant@localhost.localdomain>
2017-10-06 03:56:54 +0000

    test update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def test
-    render html: "test!!!"
+    render html: "test!!!!"
   end
 
   def hello
